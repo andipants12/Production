a872e0989be339c23d58c75c30cc71c6f257d97f
Andrea Renee Oneto <andreareneeo@gmail.com>
2017-02-11 10:58:25 -0800

    added a team count to the server

--- server/server.js
+++ server/server.js
@@ -20,11 +20,17 @@ var init = firebase.initializeApp({
   messagingSenderId: "791147440152"
 });
 /*team count init*/
-
+var fbDB = firebase.database();
 // var teamInit = {red: 1, bool: true, blue: 0};
-firebase.database().ref('team').set({red: 1, bool: true, blue: 0});
+fbDB.ref('team').set({red: 0, bool: true, blue: 0});
 
+//listener for team db path;
 
+var localTeam = {};
+fbDB.ref('team').on('value', function (snapshot) {
+  localTeam = snapshot.val();
+  console.log(localTeam, "localTeam has changed")
+})
 
 var options = {
   cert: fs.readFileSync('client-cert.pem'),
@@ -148,6 +154,24 @@ io.on('connection', function(socket) {
   });
 
 });
+
+var teamCounter = function (currentCount) { 
+  if (currentCount % 2 === 0) {
+    localTeam.blue = localTeam.blue ++;
+    fbDB.ref('team/').update({
+      blue: localTeam.blue
+    });
+  } else {
+    localTeam.red = localTeam.red ++;
+    fbDB.ref('team/').update({
+      red: localTeam.red
+    });
+  }
+  localTeam.bool = !localTeam.bool;
+  fbDB.ref('team/').update({
+    bool: localTeam.bool
+  });
+}
 
 /**************************************************************
 Server
